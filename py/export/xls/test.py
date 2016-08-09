5726b48afb5c364bd5637acaea1931db81b29301
jiaolj <841232468@qq.com>
2016-08-09 17:49:15 +0800

    Signed-off-by: jiaolj <841232468@qq.com>

--- py/export/xls/test.py
+++ py/export/xls/test.py
@@ -2,7 +2,25 @@
 from pyExcelerator import *
 w = Workbook()     #创建一个工作簿
 ws = w.add_sheet('Hey, Hades')     #创建一个工作表
-ws.write(0,0,'bit')    #在1行1列写入bit
-ws.write(0,1,u'你好')  #在1行2列写入huang
-ws.write(1,0,'xuan')   #在2行1列写入xuan
+style = XFStyle()
+style2 = XFStyle()
+fnt = Font()
+fnt2 = Font()
+
+al = Alignment()  #对齐方式
+al.horz = Alignment.HORZ_CENTER  
+al.vert = Alignment.VERT_CENTER  
+style.alignment = al 
+
+fnt.height = 200
+style.font = fnt
+
+ws.write(0,0,'bit',style)    #在1行1列写入bit
+ws.write(0,1,u'你好',style)  #在1行2列写入huang
+ws.col(0).width = 6000
+ws.col(1).width = 5000
+
+fnt2.height = 300
+style2.font = fnt2
+ws.row(0).set_style(style2)
 w.save('mini.xls')     #保存
--- py/nb/xls.py
+++ py/nb/xls.py
@@ -14,17 +14,39 @@ def getColumnName(id,cur):
         return rt[0],getSiteName(rt[1],cur)
 
 def export(rs,today,cur):
-    w = Workbook()     #创建一个工作簿
-    ws = w.add_sheet('number')     #创建一个工作表
+    w = Workbook()  #创建一个工作簿
+    ws = w.add_sheet('number')  #创建一个工作表
+    
+    style = XFStyle() #样式
+    style2 = XFStyle()
+    fnt = Font() #字体
+    fnt2 = Font()
+    al = Alignment()  #对齐方式
+    al.horz = Alignment.HORZ_CENTER  
+    al.vert = Alignment.VERT_CENTER  
+    style.alignment = al
     h = 0
-    ws.write(h,0,u'网站')    #在1行1列写入bit
-    ws.write(h,1,u'栏目')  #在1行2列写入huang
-    ws.write(h,2,u'数量')  #在1行2列写入huang
+    fnt.height = 200 #字体大小
+    style.font = fnt
+    ws.write(h,0,u'网站',style)  #写入
+    ws.write(h,1,u'栏目',style)
+    ws.write(h,2,u'数量',style)
+    fnt2.height = 300 #行高
+    style2.font = fnt2
+    ws.row(h).set_style(style)
+    
     for ret in rs:
         name,siteName = getColumnName(ret['name'],cur)
         if name:
             h+=1
-            ws.write(h,0,siteName)
-            ws.write(h,1,name)
-            ws.write(h,2,str(ret['count']))
+
+            ws.write(h,0,siteName,style) #写入
+            ws.write(h,1,name,style)
+            ws.write(h,2,ret['count'],style)
+            ws.row(h).set_style(style2)
+
+    ws.col(0).width = 10000 #列宽
+    ws.col(1).width = 6000
+    ws.col(2).width = 2000
+
     w.save(today+'.xls')     #保存
