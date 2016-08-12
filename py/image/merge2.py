a3e70b3fd120778ad1b3edaa9052ea43dc98b480
jiaolj <841232468@qq.com>
2016-08-12 13:44:11 +0800

    Signed-off-by: jiaolj <841232468@qq.com>

--- py/image/merge2.py
+++ py/image/merge2.py
@@ -1,13 +1,13 @@
 #coding:utf-8
 from PIL import Image
 #图片重叠合成
-def imgMerge(img1,img2,img3):
-    img1 = Image.open('echarts.png')
-    img2 = Image.open('echarts2.png')
+def imgMerge(image1,image2,image3):
+    img1 = Image.open(image1)
+    img2 = Image.open(image2)
     img1.paste(img2,(0,0),mask=img2) #透明背景+不透明背景
     #img  = Image.blend(img1,img2,0.5) #不透明背景+不透明背景
     #img1.show()
-    img1.save(img3)
+    img1.save(image3)
     
 if __name__=='__main__':
-    imgMerge('echarts.png','echarts.png','echarts3.png')
+    imgMerge('echarts.png','echarts2.png','echarts3.png')
